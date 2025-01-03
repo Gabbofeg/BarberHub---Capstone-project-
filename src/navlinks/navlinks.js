ce9132e037a42433b596154a45e84c97cd4db0a0
Gabriele Fagioli <gabbofeg@gmail.com>
2025-01-03 15:32:15 +0100

    v0.03

--- src/navlinks/navlinks.js
+++ src/navlinks/navlinks.js
@@ -1,6 +1,5 @@
 export const navlinks = [
     { href: '/', text: 'Home' },
-    // { href: '/chiSiamo', text: 'Chi siamo' },
     { href: '/Prodotti', text: 'Prodotti' },
     { href: '/Servizi', text: 'Servizi'},
     { href: '/Prenota', text: 'Prenota'},
